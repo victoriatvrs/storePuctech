c1d94146867ce15c502b5c35143f512c27745494
victoriatvrs <victoriaspina08@gmail.com>
2026-09-20 13:13:21 -0300

    add: added process for cash and card payments

--- model/payment/payment.py
+++ model/payment/payment.py
@@ -12,30 +12,31 @@ class Payment(ABC):
             order_id = order.order_id,
             amount = order.total(),
             method = method,
-            timestamp = datetime.now().strftime("dd-mm-yyyy %H:%M:%S"),
+            timestamp = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
         )
 
 class Cash(Payment):
     def __init__(self, tendered: float):
         self._tendered = tendered
 
-    def process(self, order: "Order") -> None:
-        # TODO: faço depois do cafézinho
-        pass
+    def process(self, order: "Order") -> Receipt:
+        receipt = self._make_receipt(order, "Cash")
+        order.advance_status()
+        return receipt
 
 class Card(Payment):
     def __init__(self, last4: str):
         self._last4 = last4
 
-    def process(self, order: "Order") -> None:
-        # TODO: faço depois do cafézinho
-        pass
+    def process(self, order: "Order") -> Receipt:
+        receipt = self._make_receipt(order, "Card")
+        order.advance_status()
+        return receipt
 
 class Pix(Payment):
     def __init__(self, key: str):
         self._key = key
 
-    # Made with Claude
     def process(self, order: "Order") -> Receipt:
         receipt = self._make_receipt(order, f"Pix: {self._key}")
         order.advance_status()
